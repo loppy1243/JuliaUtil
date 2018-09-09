013a5e9e6f5b5884c57afce570bd1693a5fba638
Nicholas Todoroff <loppy1243@gmail.com>
2018-09-08 20:53:30 -0400

    Import factorial() from SpecialFunctions

--- src/main.jl
+++ src/main.jl
@@ -1,5 +1,6 @@
 export julienne, fcat, @try_defconst, @λ, @reshape, batch, includeall, fbinom, bernoulli,
        cartesian_pow, @every
+using SpecialFunction: factorial
 
 julienne(args...; view=true) = view ? _julienne_view(args...) : _julienne_copy(args...)
 
